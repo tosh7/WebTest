30e6de846e6d117916dfc2e34e0b09299fb622cf
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2018-06-13 20:51:13 +0900

    add a collection view

---
+++ collectionView/CollectionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionViewController.swift
+//  collectionView
+//
+//  Created by Satoshi Komatsu on 2018/06/13.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ collectionView/ImageCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageCellTableViewCell.swift
+//  collectionView
+//
+//  Created by Satoshi Komatsu on 2018/06/13.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ImageCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
