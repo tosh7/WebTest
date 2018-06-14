c499da78c61e5110515ad505c8f5a80154fe9c38
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2018-06-14 16:51:12 +0900

    What happened

--- collectionView/Cells/ImageCellTableViewCell.swift
+++ collectionView/Cells/ImageCellTableViewCell.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ImageCellTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var webImage: UIImageView!
+//    let url = URL(string: "https://cover.openbd.jp/9784091898494.jpg")
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+//        let imageData = try?Data(contentsOf: url!)
+//        let image = UIImage(data: imageData!)
+//        
+//        webImage.image = image
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
