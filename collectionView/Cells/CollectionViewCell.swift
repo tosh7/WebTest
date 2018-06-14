641cccfe4f5b63ec6d7070152cd9d771ee64df69
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2018-06-14 20:58:47 +0900

    done

---
+++ collectionView/Cells/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  collectionView
+//
+//  Created by Satoshi Komatsu on 2018/06/14.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var webImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
--- collectionView/ViewControllers/CollectionViewController.swift
+++ collectionView/ViewControllers/CollectionViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController {
+class CollectionViewController: UICollectionViewController {
+    
+//    let url = URL(string: "https://cover.openbd.jp/9784091898494.jpg")
+//    var imageData: Data!
+//    var image: UIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        imageData = try?Data(contentsOf: url!)
+//        image = UIImage(data: imageData!)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Identifierが"CollectionViewCell"でCollectionViewCellというクラスのcellを取得
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        
+        // cellのlabelに色付け
+        let url = URL(string: "https://cover.openbd.jp/9784091898494.jpg")
+        let imageData = try?Data(contentsOf: url!)
+        let image = UIImage(data: imageData!
+        cell.webImage.image = image
+        
+        return cell
     }
 
 }
